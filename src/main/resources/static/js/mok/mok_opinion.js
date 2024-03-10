3ae74803e82a76f7fd6245d9abd37cb8f2905f99
Tommy <liweidesigner@gmail.com>
2024-03-10 15:00:45 +0800

    js html
    mok_opinion.js
    social_post.js
    student_socialpost.html
    
    意見回饋彈窗 的 js
    <script th:src="@{/js/mok/mok_opinion.js}"></script>

--- src/main/resources/static/js/mok/mok_opinion.js
+++ src/main/resources/static/js/mok/mok_opinion.js
@@ -9,9 +9,9 @@ $("button.btn_model_close").on("click", function(){
     $("#lightbox").addClass("none");
 });
 
-$("#lightbox").on("click", function(){
-    $("#lightbox").addClass("none");
-})
+//$("#lightbox").on("click", function(){
+//    $("#lightbox").addClass("none");
+//})
 
 $("#lightbox > article").on("click", function(e){
     e.stopPropagtion();

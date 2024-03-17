4d237084248d58f507f9492b299341db2bc030d2
Roy <royingu@gmail.com>
2024-03-17 23:55:51 +0800

    03172355

--- src/main/java/com/fitanywhere/forumpost/model/ForumPostService.java
+++ src/main/java/com/fitanywhere/forumpost/model/ForumPostService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.fitanywhere.user.model.UserService;
@@ -40,7 +41,8 @@ public class ForumPostService {
         }
 
         public List<ForumPostVO> getAll() {
-            return repository.findAll();
+        	Sort sortByUpdateTime = Sort.by(Sort.Direction.DESC, "fpUpdate");
+            return repository.findAll(sortByUpdateTime);
         }
         
         public boolean existsByFpTitle(String fpTitle) {

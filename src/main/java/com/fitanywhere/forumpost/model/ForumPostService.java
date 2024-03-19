604123454a3612166dee841cbca1ea8ba2a6fad1
Roy <royingu@gmail.com>
2024-03-19 17:27:20 +0800

    0319 1727

--- src/main/java/com/fitanywhere/forumpost/model/ForumPostService.java
+++ src/main/java/com/fitanywhere/forumpost/model/ForumPostService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fitanywhere.user.model.UserService;
 
@@ -76,6 +77,15 @@ public class ForumPostService {
         public void updateViews(int fpId, int fpViews) {
             repository.updateViews(fpId, fpViews);
         }
+        
+     // 更新社群貼文狀態 上下架 目前只用於後台
+        @Transactional
+    	public void updateforumPostStatus(Integer fpId, Integer fpStatus) {
+    	    ForumPostVO forumPost = repository.findById(fpId)
+    	    		 .orElseThrow(() -> new RuntimeException("Post not found with id " ));
+    	    forumPost.setFpStatus(fpStatus);
+    	    repository.save(forumPost);
+    	}
 
    
 }
--- src/main/java/com/fitanywhere/socialpost/controller/BackendSocialPostController.java
+++ src/main/java/com/fitanywhere/socialpost/controller/BackendSocialPostController.java
@@ -1,39 +1,21 @@
 package com.fitanywhere.socialpost.controller;
 
-import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fitanywhere.forumpost.model.ForumPostDTO;
+import com.fitanywhere.forumpost.model.ForumPostService;
+import com.fitanywhere.forumpost.model.ForumPostVO;
 import com.fitanywhere.socialpost.model.SocialPostService;
 import com.fitanywhere.socialpost.model.SocialPostUpdateStatusDTO;
 import com.fitanywhere.socialpost.model.SocialPostVO;
-import com.fitanywhere.user.model.UserService;
 
 
 @Controller
@@ -44,9 +26,14 @@ public class BackendSocialPostController {
 	@Autowired
 	SocialPostService socialPostSvc;
 	
+	@Autowired
+	ForumPostService forumPostSvc;
+	
 	@GetMapping("/backend_social")
 	public String backend_social(Model model)  {
 		
+		List<ForumPostVO> forumPosts = forumPostSvc.getAll();
+	    model.addAttribute("forumPosts", forumPosts);
 		List<SocialPostVO> socialPosts = socialPostSvc.getAll();
 	    model.addAttribute("socialPosts", socialPosts);
 
@@ -59,6 +46,17 @@ public class BackendSocialPostController {
 	public ResponseEntity<?> updateSocialPostStatus(@RequestBody SocialPostUpdateStatusDTO updateDTO) {
 	    try {
 	    	socialPostSvc.updatePostStatus(updateDTO.getSpid(), updateDTO.getSpstatus());
+	        return ResponseEntity.ok("更新成功");
+	    } catch (Exception e) {
+	        return ResponseEntity.badRequest().body("更新失敗");
+	    }
+	}
+	
+	@PostMapping("/backend/updateForumPostStatus")
+	public ResponseEntity<?> updateforumPostStatus(@RequestBody ForumPostDTO forumPostDTO) {
+	    try {
+	    	System.out.println(forumPostDTO.getFpId() +"/" +forumPostDTO.getFpStatus());
+	    	forumPostSvc.updateforumPostStatus(forumPostDTO.getFpId(), forumPostDTO.getFpStatus());
 	        return ResponseEntity.ok("更新成功");
 	    } catch (Exception e) {
 	        return ResponseEntity.badRequest().body("更新失敗");
